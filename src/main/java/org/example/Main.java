e2e36029888ec829ce65110031353655dbdde7f6
Krzysztof Kurczewski <krzysztof.kurczewski@fortum.com>
2023-10-02 13:54:26 +0200

    Attach annotation processor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import com.example.Foo;
+import com.example.Bar;
+
 @Scannable
 public class Main {
     private final String mainField = "something";
 
     public static void main(String[] args) {
-        System.out.println("Main started");
+        System.out.println("I can access: " + new Foo());
+        System.out.println("I can access Bar too: " + new Bar());
     }
 }
